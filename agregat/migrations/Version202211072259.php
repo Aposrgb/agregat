5c0f280ca6fcb6501dddcce2c6c8857a1a92ef7c
Аббос Камалов <a.kamalov@profsoft.pro>
2022-11-07 23:40:16 +0400

    FIx

--- agregat/migrations/Version202211072259.php
+++ agregat/migrations/Version202211072259.php
@@ -14,7 +14,7 @@ final class Version202211072259 extends AbstractMigration
         $this->addSql(file_get_contents(__DIR__.'/data/insert_news'));
         $this->addSql('alter sequence news_id_seq restart  with 6');
         $this->addSql('Delete from category');
-        $this->addSql(file_get_contents(__DIR__.'/data/insert_category'));
+        $this->addSql(file_get_contents(__DIR__ . '/data/insert_category.txt'));
         $this->addSql('update products set categories_id = 1 where id > 7300 and id < 7500');
         $this->addSql('update products set categories_id = 2 where id > 7500 and id < 7800');
     }
