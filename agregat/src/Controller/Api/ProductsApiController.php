4d324a3b0cf5dfb9b038c36176eb1e997727812d
Abbos Kamalov <a.kamalov@profsoft.pro>
2023-03-06 11:52:23 +0000

    Fix

--- agregat/src/Controller/Api/ProductsApiController.php
+++ agregat/src/Controller/Api/ProductsApiController.php
@@ -239,49 +239,6 @@ class ProductsApiController extends AbstractController
     }
 
     /**
-     * Заказать звонок
-     *
-     * @OA\Response(
-     *     response="200",
-     *     description="success",
-     *     @OA\JsonContent(
-     *         @OA\Property(property="message", type="string", example="ok")
-     *     )
-     * )
-     *
-     * @OA\Parameter(
-     *     in="path",
-     *     required=true,
-     *     name="phone",
-     *     @OA\Schema(type="string")
-     * )
-     *
-     */
-    #[Route('/{phone}', name: 'send_order', methods: ['PATCH'])]
-    public function sendOrder(
-        string        $phone,
-        MailerService $mailerService,
-    ): JsonResponse
-    {
-        if (!$phone or strlen($phone) < 5) {
-            throw new ApiException(message: 'Нет телефона или длина телефона слишком коротка');
-        }
-        if (str_contains($phone, '+')) {
-            if (!is_numeric(substr($phone, 1))) {
-                throw new ApiException(message: 'Неверный телефон');
-            }
-        } else {
-            if (!is_numeric($phone)) {
-                throw new ApiException(message: 'Неверный телефон');
-            }
-        }
-        $mailerService->sendMailTemplate('mail/mailer.html.twig', 'Заказали звонок АгрегатЕКБ', context: [
-            'phone' => $phone
-        ]);
-        return $this->json(data: ['message' => 'ok'], status: Response::HTTP_OK);
-    }
-
-    /**
      * Загрузка xml из 1С
      *
      * @OA\RequestBody(
--- agregat/src/Controller/IndexController.php
+++ agregat/src/Controller/IndexController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +13,6 @@ class IndexController extends AbstractController
     #[Route('', name: 'index', methods: ['GET'])]
     public function index(Request $request): Response
     {
-        $uri = $request->getUri();
-        if(str_contains($uri, '/api/')){
-            return new RedirectResponse($uri);
-        }
         return $this->render('index.html');
     }
 }
