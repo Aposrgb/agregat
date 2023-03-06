490e27ae9630cd1af070a6bf95d8ed407d13bf87
Abbos Kamalov <a.kamalov@profsoft.pro>
2023-03-06 11:48:05 +0000

    Fix

--- agregat/src/Controller/IndexController.php
+++ agregat/src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     #[Route('', name: 'index', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        echo $request->getUri();
+        if($request->getUri()){
+
+        }
         return $this->render('index.html');
     }
 }
