5cbfc73be8a21db310ee97ce59687c544028f75b
Abbos Kamalov <a.kamalov@profsoft.pro>
2023-03-24 14:21:15 +0000

    Fix

--- agregat/src/Controller/IndexController.php
+++ agregat/src/Controller/IndexController.php
@@ -9,26 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/{a}', name: 'front', methods: ['GET'])]
+    #[Route('', name: 'front', methods: ['GET'])]
     public function front(Request $request): Response
-    {
-        return $this->render('index.html');
-    }
-
-    #[Route('/{a}/{b}', name: 'front2', methods: ['GET'])]
-    public function front2(Request $request): Response
-    {
-        return $this->render('index.html');
-    }
-
-    #[Route('/{a}/{b}/{c}', name: 'front3', methods: ['GET'])]
-    public function front3(Request $request): Response
-    {
-        return $this->render('index.html');
-    }
-
-    #[Route('/{a}/{b}/{c}/{d}', name: 'front4', methods: ['GET'])]
-    public function front4(Request $request): Response
     {
         return $this->render('index.html');
     }
